581175756485b5407408cf8d70c40bff90f5135b
Alex My <alexmy@outlook.com>
2016-12-25 19:46:31 +0300

    UsersTableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(RolesTableSeeder::class);
+
+        $admin = User::firstOrNew(['username' => 'admin']);
+        $admin->displayname = 'Admin';
+        $admin->email = 'admin@pam.ru';
+        $admin->password = bcrypt('pamadminpamadmin');
+        $adminrole = Role::where('name','=','Администратор')->first();
+        $admin->role()->associate($adminrole);
+        $admin->save();
+
+        $user = User::firstOrNew(['username' => 'user']);
+        $user->displayname = 'User';
+        $user->email = 'user@pam.ru';
+        $user->password = bcrypt('pamuserpamuser');
+        $userrole = Role::where('name','=','Сотрудник')->first();
+        $user->role()->associate($userrole);
+        $user->save();
+    }
+}
