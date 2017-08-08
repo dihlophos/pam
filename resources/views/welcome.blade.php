940d0c0980f651e2cd99c50e4b36d60e872e3047
ilius33 <ilius33@mail.ru>
2017-08-08 15:05:48 +0300

    sanitary_works table migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    PAM
+                    ПЭМ
                 </div>
             </div>
         </div>
