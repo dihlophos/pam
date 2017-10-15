f86d987867fb9792ca6b69e23c495918702b21cd
vagrant <vagrant@homestead>
2017-10-15 11:49:24 +0000

    UsersTableSeeder update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,22 +12,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesTableSeeder::class);
 
         $admin = User::firstOrNew(['username' => 'admin']);
         $admin->displayname = 'Admin';
         $admin->email = 'admin@pam.ru';
         $admin->password = bcrypt(config('seeding.userpasswords.admin'));
-        $adminrole = Role::where('name','=','Администратор')->first();
-        $admin->role()->associate($adminrole);
+        $admin->is_admin = true;
         $admin->save();
 
         $user = User::firstOrNew(['username' => 'user']);
         $user->displayname = 'User';
         $user->email = 'user@pam.ru';
         $user->password = bcrypt(config('seeding.userpasswords.user'));
-        $userrole = Role::where('name','=','Сотрудник')->first();
-        $user->role()->associate($userrole);
         $user->save();
     }
 }
